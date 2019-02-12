19e1977c186e1a7e000c85439f3da2445d5e58c7
Tom <syntraprogrammeurs@gmail.com>
2019-02-12 15:26:59 +0100

    vijfde les

--- admin/includes/User.php
+++ admin/includes/User.php
@@ -6,7 +6,7 @@
  * Time: 11:42
  */
 
-class User
+class User extends Dbobject
 {
     /**VARIABELEN**/
     protected static $db_table = "users";
@@ -25,41 +25,7 @@ class User
     }
 
     /**METHODES**/
-    public static function find_this_query($sql){
-        global $database;
-        $result = $database->query($sql);
-        $the_object_array = array();
-        while($row = mysqli_fetch_array($result)){
-            $the_object_array[] = self::instantie($row);
-        }
-        return $the_object_array;
-    }
 
-    public static function find_all_users(){
-       return self::find_this_query("SELECT * FROM " . self::$db_table);
-    }
-
-    public static function find_user($id){
-        /*global $database;*/
-        $the_result_array = self::find_this_query("SELECT * FROM " . self::$db_table . " WHERE id=$id");
-        return !empty($the_result_array) ? array_shift($the_result_array) : false;
-}
-
-    public static function instantie($found_user){
-        $the_object = new self();
-        foreach($found_user as $the_attribute => $value){
-            if($the_object->has_the_attribute($the_attribute)){
-                $the_object->$the_attribute = $value;
-            }
-        }
-
-        return $the_object;
-    }
-
-    private function has_the_attribute($the_attribute){
-        $object_properties = get_object_vars($this);
-        return array_key_exists($the_attribute, $object_properties);
-    }
 
     public static function verify_user($username, $password){
         global $database;
@@ -75,64 +41,7 @@ class User
         return !empty($the_result_array) ? array_shift($the_result_array) : false;
     }
 
-    public function create(){
-        global $database;
-        $properties = $this->properties();
-
-        $sql = "INSERT INTO " . self::$db_table . " (" . implode(",",array_keys($properties)) . ") ";
-        $sql .= "VALUES ('" . implode("','",array_values($properties)) . "')";
 
 
-        if($database->query($sql)){
-            $this->id = $database->the_insert_id();
-            return true;
-        }else{
-            return false;
-        }
 
-        $database->query($sql);
-
-    }
-
-    public function update(){
-        global $database;
-
-        $sql = "UPDATE " . self::$db_table . " SET ";
-        $sql .= "username= '" . $database->escape_string($this->username) . "', " ;
-        $sql .= "password= '" . $database->escape_string($this->password) . "', ";
-        $sql .= "first_name= '" . $database->escape_string($this->first_name) . "', ";
-        $sql .= "last_name= '" . $database->escape_string($this->last_name) . "' ";
-        $sql .= " WHERE id= " . $database->escape_string($this->id);
-
-        $database->query($sql);
-        return (mysqli_affected_rows($database->connection)== 1) ? true : false;
-    }
-
-    public function delete(){
-        global $database;
-
-        $sql = "DELETE FROM " . self::$db_table . " ";
-        $sql .= "WHERE id= " . $database->escape_string($this->id);
-        $sql .= " LIMIT 1";
-
-        $database->query($sql);
-        return (mysqli_affected_rows($database->connection)== 1) ? true : false;
-    }
-
-    public function save(){
-        return isset($this->id) ? $this->update() : $this->create();
-    }
-
-    public function properties(){
-        //return get_object_vars($this);//alle variabelen van een classe worden door deze functie automatisch ingelezen
-        $properties = array();
-        foreach(self::$db_table_fields as $db_field){
-            if(property_exists($this, $db_field)){
-                $properties[$db_field] = $this->$db_field;
-            }
-        }
-        return $properties;
-
-
-    }
 }
--- admin/includes/init.php
+++ admin/includes/init.php
@@ -9,6 +9,7 @@
 require_once("functions.php");
 require_once("config.php");
 require_once("Database.php");
+require_once("Dbobject.php");
 require_once("User.php");
 require_once("Session.php");
 
